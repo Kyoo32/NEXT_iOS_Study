07fe15539029fdf7b28ca4e1f17bd3908ca645a3
Kate KyuWon <lkwon0302@gmail.com>
2015-10-14 23:27:45 +0900

    swift basic

---
+++ swiftBasic/Treehouse_playground11_comparison.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Comparison
+
+import UIKit
+
+
+
+var distance = 30
+
+if distance < 5 {
+    println("\(distance) miles is near")
+} else if distance >= 5 && distance <= 20 {
+    println("\(distance) miles is near")
+} else {
+    println("\(distance) miles is far")
+}
