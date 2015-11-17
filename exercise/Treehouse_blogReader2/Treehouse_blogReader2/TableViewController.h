018b5381893380724a36ef96e5c423e7d88f279e
Kate KyuWon <lkwon0302@gmail.com>
2015-11-17 23:07:38 +0900

    TableView from Scratch

---
+++ exercise/Treehouse_blogReader2/Treehouse_blogReader2/TableViewController.h
@@ -0,0 +1,14 @@
+//
+//  TableViewController.h
+//  Treehouse_blogReader2
+//
+//  Created by Kate KyuWon on 11/17/15.
+//  Copyright © 2015 KateKyuwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *titles;
+@end
